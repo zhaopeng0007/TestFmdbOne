775d188ac03aa9b4ed034a547c2988c2e836406a
zhaopeng <wszppygr@126.com>
2016-10-18 01:37:58 +0800

    new bug fixed

--- TestThreadFmdb/ViewController.m
+++ TestThreadFmdb/ViewController.m
@@ -19,13 +19,13 @@
     // Do any additional setup after loading the view, typically from a nib.
 //    NSDictionary *bufferDic=@{@"zp",@"lipeng"};
     
-    NSDictionary *bufferDic = @{@"key":@"adsff"};
-    
-    NSString *thisKey=[bufferDic objectForKey:@"dfasfassd"];
-    
-    [bufferDic setValue:@"DFASFSDF" forKey:thisKey];
-    
-    NSLog(@"this isdfa%@",thisKey);
+//    NSDictionary *bufferDic = @{@"key":@"adsff"};
+//    
+//    NSString *thisKey=[bufferDic objectForKey:@"dfasfassd"];
+//    
+//    [bufferDic setValue:@"DFASFSDF" forKey:thisKey];
+//    
+//    NSLog(@"this isdfa%@",thisKey);
     
 }
 
