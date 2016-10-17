7e993b67e471d948592af036a091bc06634668cc
zhaopeng <wszppygr@126.com>
2016-10-18 00:58:56 +0800

    bug fixed123

--- TestThreadFmdbTests/TestThreadFmdbTests.m
+++ TestThreadFmdbTests/TestThreadFmdbTests.m
@@ -17,24 +17,40 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSDictionary *bufferDic = @{@"key":@"adsff"};
+    
+    NSString *thisKey=[bufferDic objectForKey:@"dfasfassd"];
+    
+    [bufferDic setValue:@"DFASFSDF" forKey:thisKey];
+    
+    NSLog(@"this isdfa%@",thisKey);
+
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
     
-//    NSDictionary *bufferDic = @{@"key":@"adsff"};
-//    
-//    NSString *thisKey=[bufferDic objectForKey:@"dfasfassd"];
-//    
-//    [bufferDic setValue:@"DFASFSDF" forKey:thisKey];
-//    
-//    NSLog(@"this isdfa%@",thisKey);
+    NSDictionary *bufferDic = @{@"key":@"adsff"};
+    
+    NSString *thisKey=[bufferDic objectForKey:@"dfasfassd"];
+    
+    [bufferDic setValue:@"DFASFSDF" forKey:thisKey];
+    
+    NSLog(@"this isdfa%@",thisKey);
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    NSDictionary *bufferDic = @{@"key":@"adsff"};
+    
+    NSString *thisKey=[bufferDic objectForKey:@"dfasfassd"];
+    
+    [bufferDic setValue:@"DFASFSDF" forKey:thisKey];
+    
+    NSLog(@"this isdfa%@",thisKey);
+
 }
 
 - (void)testPerformanceExample {
